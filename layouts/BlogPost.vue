ccd8d52c23df5df484db98e4ee0430595b4582f6
BFlorian91 <b.florian91@icloud.com>
2024-02-14 14:18:43 +0100

    init nuxt-content-blog

---
+++ layouts/BlogPost.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { next, prev } = useContent();
+</script>
+
+<template>
+  <div class="container">
+    <div class="content">
+      <slot />
+    </div>
+    <div v-if="prev" class="prev-link">
+      <p>Read prev post</p>
+      <NuxtLink :to="prev._path"> prev: {{ prev.title }} </NuxtLink>
+      <p>{{ prev.description }}</p>
+    </div>
+    <div v-if="next" class="next-link">
+      <p>Read next post</p>
+      <NuxtLink :to="next._path"> Next: {{ next.title }} </NuxtLink>
+      <p>{{ next.description }}</p>
+    </div>
+  </div>
+</template>
