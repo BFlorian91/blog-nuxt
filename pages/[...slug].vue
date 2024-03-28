9ea0179a3a2c3dfadaf222c6be8fff2d2f08bcfb
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2024-03-28 23:37:36 +0100

    feat: Initiate customization of TailwindCSS prose configuration

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,10 +1,10 @@
 <template>
   <section class="min-h-screen w-full flex justify-center">
-    <div
-      class="min-h-screen w-full prose xl:w-1/2 sm:mt-20 p-10 overflow-x-auto"
+    <article
+      class="min-h-screen w-full prose prose-slate xl:w-1/2 sm:mt-20 p-10 overflow-x-auto"
     >
       <DebugHer :msg="$route.params.slug" />
       <ContentDoc />
-    </div>
+    </article>
   </section>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue,md}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -12,6 +13,22 @@ export default {
         background: "#080a08",
         debug: "#FF99CF",
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.text"),
+            h1: {
+              color: theme("colors.text"),
+            },
+            a: {
+              color: theme("colors.accent"),
+              "&:hover": {
+                color: theme("colors.accent"),
+              },
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [require("@tailwindcss/typography")],
