ce09abb79bf4c3b9907b9bfed5a116bc8446f55a
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2024-03-29 11:31:22 +0100

    feat: add seo meta

--- app.vue
+++ app.vue
@@ -2,6 +2,16 @@
 useHead({
   htmlAttrs: { lang: "en" },
   link: [{ rel: "icon", href: "/icon.png" }],
+  titleTemplate: () => `Flob blog`,
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [
+    {
+      name: "description",
+      content:
+        "This is a blog where I write about my journey as a software engineer",
+    },
+  ],
 });
 
 useSeoMeta({
@@ -9,9 +19,11 @@ useSeoMeta({
   title: "Flob Blog",
   description:
     "This is a blog where I write about my journey as a software engineer.",
-  // ogImage: "/social-image.png",
-  // twitterImage: "/social-image.png",
-  // twitterCard: "summary_large_image",
+  ogTitle: "Flob Blog",
+  ogDescription:
+    "This is a blog where I write about my journey as a software engineer.",
+  ogImage: "/favicon.ico",
+  twitterCard: "/flutter.png",
 });
 </script>
 <template>
